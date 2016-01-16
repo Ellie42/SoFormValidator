f298c0efcff570632c7a139f2972bf9e0fdf7a77
Sophie Gauthier <elait.sa1@gmail.com>
2016-01-16 10:51:42 +0000

    Fixed dist

--- gulpfile.js
+++ gulpfile.js
@@ -16,42 +16,40 @@ gulp.task('styles', function () {
     return sass('src/scss/main.scss', {style: 'expanded'})
         .pipe(sourcemaps.init())
         .pipe(autoprefixer('last 2 version'))
-        .pipe(gulp.dest('dist/assets/css'))
         .pipe(rename({suffix: '.min'}))
         .pipe(cssnano())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist/assets/css'))
         .pipe(gulp.dest('public/css'))
         .pipe(notify({message: 'Styles task complete'}));
 });
 
 gulp.task('scripts', function () {
     return gulp.src([
-            "src/js/vendor/scopedQuerySelectorShim.js",
-            "src/js/vendor/domParser.js",
-            "src/js/so-form-validator/bootstrap.js",
-            "src/js/so-form-validator/validators/validators.js",
-            "src/js/so-form-validator/validators/messages.js",
-            "src/js/so-form-validator/helper/dom-helper.js",
-            "src/js/so-form-validator/helper/form-configurator.js",
-            "src/js/so-form-validator/validator.js",
-            "src/js/so-form-validator/display/error-renderer.js",
-            "src/js/so-form-validator/so-form-validator.js",
-            "src/js/main.js"
-        ])
+        "src/js/vendor/scopedQuerySelectorShim.js",
+        "src/js/vendor/domParser.js",
+        "src/js/so-form-validator/bootstrap.js",
+        "src/js/so-form-validator/validators/validators.js",
+        "src/js/so-form-validator/validators/messages.js",
+        "src/js/so-form-validator/helper/dom-helper.js",
+        "src/js/so-form-validator/helper/form-configurator.js",
+        "src/js/so-form-validator/validator.js",
+        "src/js/so-form-validator/display/error-renderer.js",
+        "src/js/so-form-validator/so-form-validator.js",
+        "src/js/main.js"
+    ])
         .pipe(sourcemaps.init())
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(concat('so-form-validator.js'))
+        .pipe(gulp.dest('dist/js'))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(gulp.dest('dist/js'))
         .pipe(gulp.dest('public/js'))
         .pipe(notify({message: 'Scripts task complete'}));
 });
 
 gulp.task('clean', function () {
-    return del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img']);
+    return del(['dist/css', 'dist/js', 'dist/img']);
 });
 
 gulp.task('default', ['clean'], function () {
